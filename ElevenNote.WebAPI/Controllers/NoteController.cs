47a989e18d67a2ce9ad5ed602dd24ac63a36c203
Jason Henson <jason.a.henson@gmail.com>
2022-01-18 10:13:34 -0500

    end of 4.08

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -13,6 +13,12 @@ namespace ElevenNote.WebAPI.Controllers
     [Authorize]
     public class NoteController : ApiController
     {
+        public IHttpActionResult Get(int id)
+        {
+            NoteService noteService = CreateNoteService();
+            var note = noteService.GetNoteById(id);
+            return Ok(note);
+        }
         private NoteService CreateNoteService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
