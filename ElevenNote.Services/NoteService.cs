2276aa258f84435d87c57da3769fb127fe4900f5
Jason Henson <jason.a.henson@gmail.com>
2022-01-18 09:35:40 -0500

    Completed 4.03

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -17,7 +17,7 @@ namespace ElevenNote.Services
             _userId = userId; 
         }
 
-        public bool CreatNote(NoteCreate model)
+        public bool CreateNote(NoteCreate model)
         {
             var entity = new Note()
             {
